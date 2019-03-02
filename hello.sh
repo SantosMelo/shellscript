fee7bbd7e38f7be44085f09686c7f5ad57fd2825
Junior-Silva <JuniorSilva@gmail.com>
2019-03-01 23:28:18 -0300

    adicionando o arquivo hello.sh

---
+++ hello.sh
@@ -0,0 +1,1742 @@
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+#!/bin/bash
+clear
+clear:
+echo "hello world";
+exit
+:wq
+ls
+:wq
+:wq
+a
+A
+A
+A
+A
+A
+B
+B
+B
+B
+B
+B
+B
+B
+B
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+
